581e5696b6ace25dc4f1ae5d3747f42ff6d7ae25
Techy <techy05.92@gmail.com>
2022-08-31 19:44:40 -0600

    Added id's to the object

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 //objeto de jugadores
 const NBA_AllStars = 
 [
-    {year:2016, nombre:"Stephen Curry", pos:"G", team:"Golden State Warriors", selection:"Western",price: 150, url: "/Img/StephenCurry.png"},
-    {year:2016, nombre:"James Harden", pos:"SG", team:"Houston Rockets", selection:"Western" ,price: 130, url: "/Img/Harden.png"},
-    {year:2016, nombre:"Kevin Durant", pos:"SF", team:"Golden State Warriors", selection:"Western",price: 135, url: "/Img/Durant.png"},
-    {year:2016, nombre:"Kawhi Leonard", pos:"F", team:"San Antonio Spurs", selection:"Western",price: 110, url: "/Img/Kawhi.png"},
-    {year:2016, nombre:"Anthony Davis", pos:"PF", team:"New Orleans Pelicans", selection:"Western",price: 90, url: "/Img/Davis.png"},
-    {year:2016, nombre:"Russell Westbrook", pos:"G", team:"Oklahoma City Thunder", selection:"Western",price: 90, url: "/Img/Westbrook.png"},
-    {year:2016, nombre:"DeMarcus Cousins", pos:"C", team:"Sacramento Kings", selection:"Western",price: 60, url: "/Img/Cousins.png"},
-    {year:2016, nombre:"Klay Thompson", pos:"G", team:"Golden State Warriors", selection:"Western",price: 130, url: "/Img/Klay.png"},
-    {year:2016, nombre:"Draymond Green", pos:"F", team:"Golden State Warriors", selection:"Western",price: 110, url: "/Img/Draymond.png"},
-    {year:2016, nombre:"Marc Gasol", pos:"C", team:"Memphis Grizzlies", selection:"Western",price: 75, url: "/Img/MarcGasol.png"},
-    {year:2016, nombre:"DeAndre Jordan", pos:"C", team:"Los Angeles Clippers", selection:"Western",price: 75, url: "/Img/Deandre.png"},
-    {year:2016, nombre:"Gordon Hayward", pos:"GF", team:"Utah Jazz", selection:"Western",price: 60, url: "/Img/Hayward.png"},
-    {year:2016, nombre:"Kyrie Irving", pos:"G", team:"Cleveland Cavaliers", selection:"Eastern",price: 140, url: "/Img/Kyrie.png"},
-    {year:2016, nombre:"DeMar DeRozan", pos:"GF", team:"Toronto Raptors", selection:"Eastern",price: 120, url: "/Img/Demar.png"},
-    {year:2016, nombre:"LeBron James", pos:"F", team:"Cleveland Cavaliers", selection:"Eastern",price: 150, url: "/Img/Lebron.png"},
-    {year:2016, nombre:"Giannis Antetokounmpo", pos:"F", team:"Milwaukee Bucks", selection:"Eastern",price: 115, url: "/Img/Giannis.png"},
-    {year:2016, nombre:"Jimmy Butler", pos:"GF", team:"Chicago Bulls", selection:"Eastern",price: 100, url: "/Img/Butler.png"},
-    {year:2016, nombre:"Isaiah Thomas", pos:"PG", team:"Boston Celtics", selection:"Eastern",price: 80, url: "/Img/IsaiahT.png"},
-    {year:2016, nombre:"John Wall", pos:"PG", team:"Washington Wizards", selection:"Eastern",price: 85, url: "/Img/JohnWall.png"},
-    {year:2016, nombre:"Kevin Love", pos:"FC", team:"Cleveland Cavaliers", selection:"Eastern",price: 98, url: "/Img/KevinLove.png"},
-    {year:2016, nombre:"Kyle Lowry", pos:"PG", team:"Toronto Raptors", selection:"Eastern",price: 70, url: "/Img/Lowry.png"},
-    {year:2016, nombre:"Paul George", pos:"GF", team:"Indiana Pacers", selection:"Eastern",price: 75, url: "/Img/PaulGeorge.png"},
-    {year:2016, nombre:"Kemba Walker", pos:"G", team:"Charlotte Hornets", selection:"Eastern",price: 65, url: "/Img/Kemba.png"},
-    {year:2016, nombre:"Paul Millsap", pos:"FC", team:"Atlanta Hawks", selection:"Eastern",price: 40, url: "/Img/Millsap.png"},
+    {id:1,year:2016, nombre:"Stephen Curry", pos:"G", team:"Golden State Warriors", selection:"Western",price: 150, url: "/Img/StephenCurry.png"},
+    {id:2,year:2016, nombre:"James Harden", pos:"SG", team:"Houston Rockets", selection:"Western" ,price: 130, url: "/Img/Harden.png"},
+    {id:3,year:2016, nombre:"Kevin Durant", pos:"SF", team:"Golden State Warriors", selection:"Western",price: 135, url: "/Img/Durant.png"},
+    {id:4,year:2016, nombre:"Kawhi Leonard", pos:"F", team:"San Antonio Spurs", selection:"Western",price: 110, url: "/Img/Kawhi.png"},
+    {id:5,year:2016, nombre:"Anthony Davis", pos:"PF", team:"New Orleans Pelicans", selection:"Western",price: 90, url: "/Img/Davis.png"},
+    {id:6,year:2016, nombre:"Russell Westbrook", pos:"G", team:"Oklahoma City Thunder", selection:"Western",price: 90, url: "/Img/Westbrook.png"},
+    {id:7,year:2016, nombre:"DeMarcus Cousins", pos:"C", team:"Sacramento Kings", selection:"Western",price: 60, url: "/Img/Cousins.png"},
+    {id:8,year:2016, nombre:"Klay Thompson", pos:"G", team:"Golden State Warriors", selection:"Western",price: 130, url: "/Img/Klay.png"},
+    {id:9,year:2016, nombre:"Draymond Green", pos:"F", team:"Golden State Warriors", selection:"Western",price: 110, url: "/Img/Draymond.png"},
+    {id:10,year:2016, nombre:"Marc Gasol", pos:"C", team:"Memphis Grizzlies", selection:"Western",price: 75, url: "/Img/MarcGasol.png"},
+    {id:11,year:2016, nombre:"DeAndre Jordan", pos:"C", team:"Los Angeles Clippers", selection:"Western",price: 75, url: "/Img/Deandre.png"},
+    {id:12,year:2016, nombre:"Gordon Hayward", pos:"GF", team:"Utah Jazz", selection:"Western",price: 60, url: "/Img/Hayward.png"},
+    {id:13,year:2016, nombre:"Kyrie Irving", pos:"G", team:"Cleveland Cavaliers", selection:"Eastern",price: 140, url: "/Img/Kyrie.png"},
+    {id:14,year:2016, nombre:"DeMar DeRozan", pos:"GF", team:"Toronto Raptors", selection:"Eastern",price: 120, url: "/Img/Demar.png"},
+    {id:15,year:2016, nombre:"LeBron James", pos:"F", team:"Cleveland Cavaliers", selection:"Eastern",price: 150, url: "/Img/Lebron.png"},
+    {id:16,year:2016, nombre:"Giannis Antetokounmpo", pos:"F", team:"Milwaukee Bucks", selection:"Eastern",price: 115, url: "/Img/Giannis.png"},
+    {id:17,year:2016, nombre:"Jimmy Butler", pos:"GF", team:"Chicago Bulls", selection:"Eastern",price: 100, url: "/Img/Butler.png"},
+    {id:18,year:2016, nombre:"Isaiah Thomas", pos:"PG", team:"Boston Celtics", selection:"Eastern",price: 80, url: "/Img/IsaiahT.png"},
+    {id:19,year:2016, nombre:"John Wall", pos:"PG", team:"Washington Wizards", selection:"Eastern",price: 85, url: "/Img/JohnWall.png"},
+    {id:20,year:2016, nombre:"Kevin Love", pos:"FC", team:"Cleveland Cavaliers", selection:"Eastern",price: 98, url: "/Img/KevinLove.png"},
+    {id:21,year:2016, nombre:"Kyle Lowry", pos:"PG", team:"Toronto Raptors", selection:"Eastern",price: 70, url: "/Img/Lowry.png"},
+    {id:22,year:2016, nombre:"Paul George", pos:"GF", team:"Indiana Pacers", selection:"Eastern",price: 75, url: "/Img/PaulGeorge.png"},
+    {id:23,year:2016, nombre:"Kemba Walker", pos:"G", team:"Charlotte Hornets", selection:"Eastern",price: 65, url: "/Img/Kemba.png"},
+    {id:24,year:2016, nombre:"Paul Millsap", pos:"FC", team:"Atlanta Hawks", selection:"Eastern",price: 40, url: "/Img/Millsap.png"},
 ]
 
 //arreglo para registrar a los usuarios
@@ -54,15 +54,9 @@ document.addEventListener('click', e=> {
 
 document.body.onload = OnPageLoad;
 
-// const nav_Teams = document.getElementById("nav_Teams");
-// //nav_Teams.onclick = TeamsUnique;
-
-// const nav_Player =document.getElementById("nav_Player");
-// //nav_Player.onclick = get players by year
-
-// const nav_Position = document.getElementById("nav_Position");
-// //nav_Position.onclick = get available positions
-
+//when clicking on specific position
+const PosClick = document.getElementsByClassName("link");
+PosClick.onclick = PositionClicked(this.id) 
 
 //registra nombre de usuario en el arreglo
 function RegUsuario()
@@ -152,7 +146,7 @@ function PositionUnique()
     Positions.forEach(element => 
     {
         let PosList = document.createElement("linkTeam")
-        PosList.innerHTML = `<a href="#" class="link">${element}</a>`;
+        PosList.innerHTML = `<a id="${element}" class="link">${element}</a>`;
         let Pos = document.getElementById("Pos")
         Pos.appendChild(PosList);
     });
@@ -185,7 +179,7 @@ function CreateTable()
                 <div class="selection">${datos.selection}</div>
                 <div class="position">${datos.pos}</div>
                 <div class="year">${datos.year}</div>
-                <button class="btn_Buy">Agregar al carrito</button>
+                <button id=${datos.id} class="btn_Buy">Agregar al carrito</button>
             </div>
         </div>
         </td>`
@@ -201,4 +195,9 @@ function CreateTable()
     tableNBA.innerHTML = StringHTML;
     let cuerpo = document.getElementById("cuerpo");
     cuerpo.appendChild(tableNBA);
+}
+
+function PositionClicked(id)
+{
+    //unfinished
 }
